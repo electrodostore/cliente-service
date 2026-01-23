4739c1dae02e3d161e858246e530caaa4cb32e67
Emanuel <emanuelatencia1126654@correo.itm.edu.co>
2026-01-23 08:25:29 -0500

    Modelado de datos para la entidad Cliente de la base de datos electrodostore_cliente

---
+++ src/main/java/com/electrodostore/cliente_service/model/Cliente.java
@@ -0,0 +1,26 @@
+package com.electrodostore.cliente_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter  @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cellphone;
+    private String document;
+    private String address;
+
+}
